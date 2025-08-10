4d58bf22bda4bf6673cc6a376f2e0e0e03cc772c
SerheyBr <sergeydrilkov@mail.ru>
2025-08-10 13:11:42 +0300

    add header

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,25 @@
+// action berger menu
+const btnBurger = document.querySelector(".burger-menu");
+const burgerMenu = document.querySelector(".header__navigation");
+
+btnBurger.addEventListener("click", (e) => {
+  e.currentTarget.classList.toggle("active");
+  burgerMenu.classList.toggle("active");
+});
+
+const allLiInBurgerMenu = burgerMenu.querySelectorAll("ul li");
+
+allLiInBurgerMenu.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    const element = e.currentTarget;
+    if (element.querySelector("ul")) {
+      element.querySelector("ul").classList.toggle("active");
+    } else {
+      console.log("no");
+    }
+  });
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   new SlimSelect({
     select: ".my-form .wpcf7-select",
